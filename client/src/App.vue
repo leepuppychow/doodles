b2da3a3c384e93229d22a515bae4b0f1a5dd788f
leepuppychow <leepuppychow@gmail.com>
2020-06-07 09:56:00 -0600

    Init client service using Vue

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Shared Canvas</h1>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    console.log("Hello")
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+canvas {
+  width: 500px;
+  height: 500px;
+  border: solid black 1px;
+}
+</style>
