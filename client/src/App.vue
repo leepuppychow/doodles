e35c62d7ff54edece8344c51fbd58e05c4f04813
leepuppychow <leepuppychow@gmail.com>
2020-06-07 10:52:05 -0600

    Adds basic drawing and canvas resizing functionality

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,60 @@
 <template>
   <div id="app">
     <h1>Welcome to Shared Canvas</h1>
-    <canvas></canvas>
+    <canvas
+      ref="canvas"
+      :width="canvasWidth"
+      :height="canvasHeight"
+      @mousedown="isDrawing = true"
+      @mouseup="isDrawing = false"
+      @mousemove="draw"
+    ></canvas>
   </div>
 </template>
 
 <script>
+const CANVAS_WIDTH_PERCENT = 0.7;
+const CANVAS_HEIGHT_PERCENT = 0.6;
+
 export default {
   name: 'App',
+  data() {
+    return {
+      isDrawing: false,
+      cursorX: 0,
+      cursorY: 0,
+      canvasWidth: 0,
+      canvasHeight: 0,
+    }
+  },
   created() {
-    console.log("Hello")
-  }
+    window.addEventListener('resize', this.resizeCanvas);
+    this.resizeCanvas();
+  },
+  methods: {
+    resizeCanvas() {
+      this.canvasWidth = window.innerWidth * CANVAS_WIDTH_PERCENT;
+      this.canvasHeight = window.innerHeight * CANVAS_HEIGHT_PERCENT;
+    },
+    draw(event) {
+      if (!this.isDrawing) return;
+
+      const { canvas } = this.$refs;
+      const context = canvas.getContext('2d');
+      const canvasPosition = canvas.getBoundingClientRect();
+
+      context.beginPath();
+      context.lineWidth = 5;
+      context.lineCap = 'round';
+      context.strokeStyle = 'green';
+
+      this.cursorX = event.clientX - canvasPosition.left;
+      this.cursorY = event.clientY - canvasPosition.top;
+
+      context.lineTo(this.cursorX, this.cursorY);
+      context.stroke();
+    }
+  },
 }
 </script>
 
@@ -21,12 +65,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 canvas {
-  width: 500px;
-  height: 500px;
   border: solid black 1px;
 }
 </style>
